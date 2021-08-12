401b5e361b58406a1eed2a5f284759235eda264d
DGibeau <43452822+DGibeau@users.noreply.github.com>
2021-08-12 05:03:49 -0700

    Add files via upload

--- Dungeon.py
+++ Dungeon.py
@@ -1,44 +1,39 @@
 while True:
     import random
-    game_over = False
-    new_monster = True
-    dungeon_loop = False
-    already_debuffed = False
-    restart_boolean = ''
-    quit_boolean = ''
-    restart_once = 0
-    quit_once = 0
+
+    class Game:
+        game_over = False
+        new_monster = True
+        dungeon_loop = False
+        already_poisoned = False
+        already_debuffed = False
+        restart_boolean = ''
+        quit_boolean = ''
+        restart_once = 0
+        quit_once = 0
 
     class Player(object):
         poisoned = False
         debuffed = False
         gold = 0
-        armor = 0
-        poison_resist = False
-        debuff_resist = False
-        strength = 0
         inventory = []
 
-        def __init__(self, max_hp, hp, max_ap, ap):
+        def __init__(self, max_hp, hp, max_ap, ap, weapon, armor):
             self.max_hp = max_hp
             self.hp = hp
             self.max_ap = max_ap
             self.ap = ap
-
-    player = Player(20, 20, 10, 10)
-
+            self.weapon = weapon
+            self.armor = armor
 
     class PlayerAttack(object):
 
-        def __init__(self, name, min_dmg, max_dmg, accuracy, ap_cost):
+        def __init__(self, name, min_dmg, max_dmg, crit_chance, ap_cost):
             self.name = name
             self.min_dmg = min_dmg
             self.max_dmg = max_dmg
-            self.accuracy = accuracy
+            self.crit_chance = crit_chance
             self.ap_cost = ap_cost
-
-    slash = PlayerAttack("Slash", player.strength + 2, player.strength + 3, 100, 1)
-    shredding_strike = PlayerAttack("Shredding Strike", player.strength + 4, player.strength + 5, 95, 3)
 
     class Armor(object):
 
@@ -49,19 +44,27 @@ while True:
             self.poison_resist = poison_resist
             self.debuff_resist = debuff_resist
 
-    gambeson = Armor("Gambeson", 1, 10, False, False)
-    chain_mail = Armor("Chain Mail Armor", 2, 20, True, False)
-    plate_armor = Armor("Plate Armor", 3, 30, True, True)
+    # Armor types
+    cloth = Armor("Cloth", 1, 0, False, False)
+    gambeson = Armor("Gambeson", 0.92, 15, False, False)
+    chain_mail = Armor("Chain Mail Armor", 0.9, 30, True, False)
+    plate_armor = Armor("Plate Armor", 0.88, 50, True, True)
 
     class Weapon(object):
 
-        def __init__(self, name, dmg, cost, crit_chance):
+        def __init__(self, name, bonus_dmg, cost, crit_dmg1, crit_dmg2, crit_chance):
             self.name = name
-            self.dmg = dmg
+            self.bonus_dmg = bonus_dmg
             self.cost = cost
+            self.crit_dmg1 = crit_dmg1
+            self.crit_dmg2 = crit_dmg2
             self.crit_chance = crit_chance
 
-    short_sword = Weapon("Short Sword", 1, 20, 1)
+    # weapon types
+    dagger = Weapon("Dagger", 0, 0, 1, 3, 10)
+    short_sword = Weapon("Short Sword", 3, 20, 3, 5, 10)
+    battle_axe = Weapon("Battle Axe", 5, 40, 7, 9, 10)
+    long_sword = Weapon("Long Sword", 9, 60, 9, 11, 10)
 
     # ----------------------------------------------
 
@@ -69,6 +72,8 @@ while True:
     # I created a monster list to be able to get a random monster from it
         species = 'monster'
         gold_drop = 1
+        min_poison = 7
+        max_poison = 10
         monster_list = []
 
         def __init__(self, name, max_hp, hp, min_dmg, max_dmg, accuracy, poison, debuff):
@@ -82,47 +87,69 @@ while True:
             self.debuff = debuff
             __class__.monster_list.append(self)
 
-    goblin = Monster("Goblin", 8, 8, 1, 3, 1, False, False)
-    spider = Monster("Giant Spider", 7, 7, 1, 2, 1, False, True)
-    orc = Monster("Orc", 15, 15, 3, 5, 2, False, False)
-    skeleton = Monster("Skeleton", 9, 9, 2, 3, 1, False, False)
-    slime = Monster("Slime", 13, 13, 1, 1, 1, True, False)
-    zombie = Monster("Zombie", 8, 8, 1, 3, 2, True, True)
+    # Monster types
+    goblin = Monster("Goblin", 80, 80, 10, 30, 90, False, False)
+    spider = Monster("Giant Spider", 70, 70, 10, 20, 90, False, True)
+    orc = Monster("Orc", 110, 110, 25, 40, 85, False, False)
+    skeleton = Monster("Skeleton", 90, 90, 20, 30, 90, False, False)
+    slime = Monster("Slime", 105, 105, 10, 15, 90, True, False)
+    zombie = Monster("Zombie", 80, 80, 10, 30, 80, True, True)
 
     rand_index = random.randrange(len(Monster.monster_list))
     rand_monster = Monster.monster_list[rand_index]
+
+    player = Player(120, 120, 10, 10, dagger, cloth)
+
+    # Attack types
+    slash = PlayerAttack("Slash", 15, 25, 10, 1)
+    shredding_strike = PlayerAttack("Shredding Strike", 30, 40, 5, 3)
 
     #print("This {} is a {} and its hp is {}.".format(rand_monster.species, rand_monster.name, rand_monster.hp))
 
     # Functions----------------------------------------------------------------
     def player_info():
-        print("HP: {}/{}   Armor: {}\nAP: {}/{}   Gold: {}".format(player.hp, player.max_hp, player.armor
+        print("HP: {}/{}  Armor: {}%\nAP: {}/{}    Gold: {}".format(player.hp, player.max_hp,
+                                                                    int((1 - player.armor.protection) * 100)
                                                             ,player.ap, player.max_ap, player.gold))
         alt_line()
 
-    def monster_info():
-        global new_monster
+    def player_damage(ap_cost, min_dmg, max_dmg, attack_name):
+        if player.ap >= ap_cost:
+            player.ap -= ap_cost
+            crit_check = random.randint(1, 100)
+            if crit_check <= player.weapon.crit_chance:
+                crit_dmg = random.randint(player.weapon.crit_dmg1, player.weapon.crit_dmg2)
+                player_dmg = random.randint(min_dmg, max_dmg) + player.weapon.bonus_dmg + crit_dmg
+                rand_monster.hp -= player_dmg
+                print("************************\nCRITICAL HIT!\n************************\n"
+                      "You used {} and did {} damage".format(attack_name, player_dmg))
+            else:
+                player_dmg = random.randint(min_dmg, max_dmg) + player.weapon.bonus_dmg
+                rand_monster.hp -= player_dmg
+                print("You used {} and did {} damage".format(attack_name, player_dmg))
+        else:
+            print("Not enough AP")
 
-        if new_monster is True:
+    def monster_info():
+        if Game.new_monster is True:
             print("A {} has appeared!\n".format(rand_monster.name))
-            new_monster = False
+            Game.new_monster = False
 
         print("{} HP: {}/{}".format(rand_monster.name, rand_monster.hp, rand_monster.max_hp))
 
     def monster_accuracy():
-        monster_acc = random.randint(1, 10)
-        if monster_acc > rand_monster.accuracy:
+        monster_acc = random.randint(1, 100)
+        if monster_acc <= rand_monster.accuracy:
             return True
         else:
             return False
 
     def monster_respawn():
-        global new_monster
         global rand_monster
         global rand_index
 
-        if rand_monster.hp <= 0 or new_monster is True:
-            new_monster = True
+        if rand_monster.hp <= 0 or Game.new_monster is True:
+            Game.new_monster = True
             rand_monster.hp = rand_monster.max_hp
             rand_index = random.randrange(len(Monster.monster_list))
             rand_monster = Monster.monster_list[rand_index]
@@ -130,34 +157,38 @@ while True:
     def monster_damage():
         if rand_monster.hp > 0:
             if monster_accuracy() is True:
-                monster_dmg = random.randint(rand_monster.min_dmg, rand_monster.max_dmg)
+                monster_dmg = round(random.randint(rand_monster.min_dmg, rand_monster.max_dmg) * player.armor.protection)
                 player.hp -= monster_dmg
                 print("You have taken {} damage from the {}".format(monster_dmg, rand_monster.name))
                 if rand_monster.poison is True:
-                    poison_acc = random.randint(1, 10)
-                    # 1 + 5 makes it a 40% chance 2 + 5 = 30%
-                    if poison_acc > rand_monster.accuracy + 5:
+                    poison_acc = random.randint(1, 100)
+                    # 90 - 55 = 35% | 80 - 55 = 25%
+                    if poison_acc <= rand_monster.accuracy - 55:
                         player.poisoned = True
-                        print("The {} poisoned you (-1 hp every turn)".format(rand_monster.name))
+                        print("The {} poisoned you (-{} to {} HP every turn)".format(rand_monster.name,
+                                                    rand_monster.min_poison, rand_monster.max_poison))
                         poison_func()
                 if rand_monster.debuff is True:
-                    debuff_acc = random.randint(1, 10)
-                    if debuff_acc > rand_monster.accuracy + 5:
+                    debuff_acc = random.randint(1, 100)
+                    if debuff_acc <= rand_monster.accuracy - 55:
                         player.debuffed = True
-                        print("The {} weakened you (-1 armor and -3 max AP)".format(rand_monster.name))
                         debuff_func()
             else:
                 print("The {}'s attack missed!".format(rand_monster.name))
 
     def poison_func():
         def poison_apply():
-            player.hp -= 1
-            print("You have taken 1 damage from being poisoned")
+            if Game.already_poisoned is False:
+                Game.already_poisoned = True
+                poison_dmg = random.randint(rand_monster.min_poison, rand_monster.max_poison)
+                player.hp -= poison_dmg
+                print("You have taken {} damage from being poisoned".format(poison_dmg))
 
         if player.poisoned is True:
-            if player.poison_resist is True:
-                rand_resist = random.randint(1, 10)
-                if rand_resist > 8:
+            if player.armor.poison_resist is True:
+                rand_resist = random.randint(1, 100)
+                # 20% chance
+                if rand_resist <= 20:
                     player.poisoned = False
                     print("You resisted the poison")
                 else:
@@ -168,24 +199,21 @@ while True:
 
     def debuff_func():
         def debuff_apply():
-            global already_debuffed
-
-            if already_debuffed is False:
-                already_debuffed = True
-                player.max_ap -= 3
+            if Game.already_debuffed is False:
+                Game.already_debuffed = True
+                ap_loss = 3
+                armor_loss = 0.08
+                player.max_ap -= ap_loss
+                player.armor.protection += armor_loss
+                print("The {} weakened you (-{}% Armor and -{} Max AP)"
+                      .format(rand_monster.name, int(armor_loss * 100), ap_loss))
                 if player.ap > player.max_ap:
                     player.ap = player.max_ap
-                if player.armor <= 0:
-                    print("Your max AP has been reduced to {}".format(player.max_ap))
-                else:
-                    player.armor -= 1
-                    print("Your armor has been reduced to {}\nYour max AP has been reduced to {}"
-                          .format(player.armor, player.max_ap))
 
         if player.debuffed is True:
-            if player.debuff_resist is True:
-                rand_resist = random.randint(1, 10)
-                if rand_resist > 8:
+            if player.armor.debuff_resist is True:
+                rand_resist = random.randint(1, 100)
+                if rand_resist <= 20:
                     player.debuffed = False
                     print("You resisted the weaken")
                 else:
@@ -194,36 +222,26 @@ while True:
                 debuff_apply()
 
     def recover():
-        global already_debuffed
-
         if player.poisoned is True:
-            rand_recover = random.randint(1,10)
-            if rand_recover > 8:
+            rand_recover = random.randint(1, 100)
+            if rand_recover <= 20:
                 player.poisoned = False
+                Game.already_poisoned = False
                 print("You recovered from the poison")
 
         if player.debuffed is True:
-            rand_recover = random.randint(1, 10)
-            if rand_recover > 8:
+            rand_recover = random.randint(1, 100)
+            if rand_recover <= 20:
                 player.debuffed = False
-                already_debuffed = False
+                Game.already_debuffed = False
                 player.max_ap = 10
-                player.armor = 0
+                player.armor.protection += 0.08
                 print("You recovered from being weakened")
-
-    def player_damage(ap_cost, min_dmg, max_dmg, attack_name):
-        if player.ap >= ap_cost:
-            player.ap -= ap_cost
-            player_dmg = random.randint(min_dmg, max_dmg)
-            rand_monster.hp -= player_dmg
-            print("You used {} and did {} damage".format(attack_name, player_dmg))
-        else:
-            print("Not enough AP")
 
     def rewards():
         rand_monster.gold_drop = random.randint(1, 5)
         player.gold += rand_monster.gold_drop
-        rand_hp = random.randint(2, 3)
+        rand_hp = random.randint(5, 20)
         player.hp += rand_hp
         rand_ap = random.randint(0, 1)
         player.ap += rand_ap
@@ -263,10 +281,8 @@ while True:
         line()
 
     def dungeon():
-        global dungeon_loop
-
         print("You entered the dungeon")
-        while dungeon_loop is True:
+        while Game.dungeon_loop is True:
             while True:
                 monster_info()
                 alt_line()
@@ -301,7 +317,7 @@ while True:
                     player_input = input("1: Continue in the dungeon\n2: Go back to town\n> ")
                     if player_input == "2":
                         continue_loop = False
-                        dungeon_loop = False
+                        Game.dungeon_loop = False
                     elif player_input == "1":
                         continue_loop = False
                         line()
@@ -315,9 +331,9 @@ while True:
             ap_gain()
 
     def armor_shop():
-        shop_loop = True
-        while shop_loop is True:
+        while True:
             player_info()
+            # ADD PRICES ##################################
             player_input = input("Welcome to the Armor Shop!\nWhat will you buy?\n1: Gambeson Armor\n"
                                  "2: Chain Mail Armor\n3: Plate Armor\nq: Go back to town\n> ")
             if player_input == "1":
@@ -348,13 +364,12 @@ while True:
                 else:
                     no_gold()
             elif player_input.lower() == "q":
-                shop_loop = False
+                break
             else:
                 invalid_option()
 
     def weapon_shop():
-        shop_loop = True
-        while shop_loop is True:
+        while True:
             player_info()
             player_input = input("Welcome to the Weapon Shop!\nWhat will you buy?\n1: Gambeson Armor\n"
                                  "2: Chain Mail Armor\n3: Plate Armor\nq: Go back to town\n> ")
@@ -386,14 +401,12 @@ while True:
                 else:
                     no_gold()
             elif player_input.lower() == "q":
-                shop_loop = False
+                break
             else:
                 invalid_option()
 
     def town():
-        global dungeon_loop
-
-        if dungeon_loop is False:
+        if Game.dungeon_loop is False:
             town_loop = True
             while town_loop is True:
                 player_input = input("Welcome to the small village of Seronia!\nWhere will you go?\n1: Armor Shop\n"
@@ -407,60 +420,51 @@ while True:
                     print("Welcome to the Potion Shop")
                 elif player_input == "4":
                     town_loop = False
-                    dungeon_loop = True
+                    Game.dungeon_loop = True
                     dungeon()
                 elif player_input.lower() == "q":
-                    quit()
-                    if quit_boolean is True:
+                    quit_func()
+                    if Game.quit_boolean is True:
                         break
                 else:
                     invalid_option()
                 line()
 
     def restart():
-        global game_over
-        global dungeon_loop
-        global restart_once
-        global restart_boolean
-
         if player.hp <= 0:
-            while restart_once == 0:
+            while Game.restart_once == 0:
                 player_input = input("You died\nRestart? (y/n)\n> ")
                 if player_input.lower() == "n":
-                    game_over = True
-                    dungeon_loop = False
-                    restart_boolean = False
-                    restart_once = 1
+                    Game.game_over = True
+                    Game.dungeon_loop = False
+                    Game.restart_boolean = False
+                    Game.restart_once = 1
                     break
                 elif player_input.lower() == "y":
-                    dungeon_loop = False
-                    restart_boolean = True
-                    restart_once = 1
+                    Game.dungeon_loop = False
+                    Game.restart_boolean = True
+                    Game.restart_once = 1
                     break
                 else:
                     invalid_option()
-        return restart_boolean
+        return Game.restart_boolean
 
-    def quit():
-        global quit_once
-        global quit_boolean
-        global game_over
-
-        while quit_once == 0:
+    def quit_func():
+        while Game.quit_once == 0:
             player_input = input("Are you sure? (y/n)\n> ")
             if player_input.lower() == "y":
-                game_over = True
-                quit_boolean = True
-                quit_once = 1
+                Game.game_over = True
+                Game.quit_boolean = True
+                Game.quit_once = 1
             elif player_input.lower() == "n":
-                quit_boolean = False
-                quit_once = 1
+                Game.quit_boolean = False
+                Game.quit_once = 1
             else:
                 invalid_option()
-        quit_once = 0
+        Game.quit_once = 0
 
     # Game Start------------------------------------------------------------------
-    while game_over is False:
+    while Game.game_over is False:
         if restart() is True or False:
             break
         town()
@@ -471,4 +475,6 @@ while True:
 -after a certain amount of defeated enemies, player is able to choose a different path
 -passive money making building to purchase in town
 -merchant who can upgrade your max hp and ap
+-Rob the shop but can't use shops for a certain period of time
+-Add a tavern where you can gamble
 """
